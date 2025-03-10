9aa29485f460ceb3e632c81d843abfe98250af34
kark2022 <115482608+kark2022@users.noreply.github.com>
2025-03-10 09:41:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 # Create S3 Bucket resource
-resource "aws_s3_bucket" "demos_buckets" {
+resource "aws_s3_bucket" "demos-buckets" {
   bucket = var.bucket_name
   force_destroy = true
 }
 
 # Configure bucket acl
-resource "aws_s3_bucket_ownership_controls" "demos_buckets" {
-  bucket = aws_s3_bucket.demos_buckets.id
+resource "aws_s3_bucket_ownership_controls" "demos-buckets" {
+  bucket = aws_s3_bucket.demos-buckets.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
-resource "aws_s3_bucket_public_access_block" "demos_buckets" {
-  bucket = aws_s3_bucket.demos_buckets.id
+resource "aws_s3_bucket_public_access_block" "demos-buckets" {
+  bucket = aws_s3_bucket.demos-buckets.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -20,19 +20,19 @@ resource "aws_s3_bucket_public_access_block" "demos_buckets" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "demos_buckets" {
+resource "aws_s3_bucket_acl" "demos-buckets" {
   depends_on = [
-    aws_s3_bucket_ownership_controls.demos_buckets,
-    aws_s3_bucket_public_access_block.demos_buckets,
+    aws_s3_bucket_ownership_controls.demos-buckets,
+    aws_s3_bucket_public_access_block.demos-buckets,
   ]
 
-  bucket = aws_s3_bucket.demos_buckets.id
+  bucket = aws_s3_bucket.demos-buckets.id
   acl    = "public-read"
 }
 
 # Define website configuration
-resource "aws_s3_bucket_website_configuration" "demos_buckets" {
-  bucket = aws_s3_bucket.demos_buckets.id
+resource "aws_s3_bucket_website_configuration" "demos-buckets" {
+  bucket = aws_s3_bucket.demos-buckets.id
 
   index_document {
     suffix = "index.html"
@@ -44,7 +44,7 @@ resource "aws_s3_bucket_website_configuration" "demos_buckets" {
 }
 # Configure bucket policy
 resource "aws_s3_bucket_policy" "policy" {
-  bucket = aws_s3_bucket.demos_buckets.id
+  bucket = aws_s3_bucket.demos-buckets.id
   policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -57,7 +57,7 @@ resource "aws_s3_bucket_policy" "policy" {
                 "s3:GetObject"
             ],
             "Resource": [
-                "arn:aws:s3:::${aws_s3_bucket.demos_buckets.id}/*"
+                "arn:aws:s3:::${aws_s3_bucket.demos-buckets.id}/*"
             ]
         }
     ]
@@ -69,7 +69,7 @@ EOF
 resource "aws_s3_object" "webapp" {
   acl          = "public-read"
   key          = "index.html"
-  bucket       = aws_s3_bucket.demos_buckets.id
+  bucket       = aws_s3_bucket.demos-buckets.id
   content      = file("${path.module}/index.html")
   content_type = "text/html"
 }
